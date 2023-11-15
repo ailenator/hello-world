ddbcc52e10e150b2a8d39babc0a7cae5420e7815
Abid Syed <1220570+abyssidian@users.noreply.github.com>
2023-11-15 17:27:45 -0600

    Third commit

--- http_server.js
+++ http_server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app     = express();
+const port    = process.env.port || 3000;
 
 app.get('/', function(req, rest){
   rest.send('hello world!');
 });
 
-app.listen(80, function(){
-  console.log('Running on port 80');
+app.listen(port, err => {
+    if(err) throw err;
+    console.log(`Running on port ${port}`);
 });
